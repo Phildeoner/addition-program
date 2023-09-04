66087cfe255d43fb474c07f47a8284a69ad1ac29
Yakubu Bobai Ephraim <phildeoner@gmail.com>
2023-09-04 10:43:41 +0100

    Define inputs and addition

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,1 +1,7 @@
 const prompt = require("prompt-sync")();
+
+let firstInput = prompt("Insert your first number: ");
+let secondInput = prompt("Insert your second number: ");
+
+let addition = firstInput + secondInput;
+console.log(addition);
